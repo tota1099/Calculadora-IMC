1a97a2d8c4c860b920de17e2a1effb190dc81266
Renan Porto <renan.porto1099@gmail.com>
2018-10-13 12:50:05 -0300

    Ajustando layout do aplicativo e verificando entradas

--- App.js
+++ App.js
@@ -12,6 +12,7 @@ import {
   Text, 
   TextInput, 
   TouchableOpacity,
+  Alert,
   View
 }  from 'react-native';
 
@@ -25,37 +26,66 @@ export default class App extends Component<Props> {
   }
 
   calcular(){
+
+    const regex_input = /^[0-9.]+$/
     
-    let imc = this.state.massa / (this.state.altura * this.state.altura)
+    if (this.state.massa && this.state.altura){
 
-    let state = this.state
-    state.resultado = imc
-    this.setState(state)
+      if ( regex_input.test(this.state.massa) && regex_input.test(this.state.altura) ){
+        let imc = this.state.massa / (this.state.altura * this.state.altura)
 
-    if ( state.resultado < 16 ){
-      state.resultadoText = "Magreza Grave"
+        let state = this.state
+        state.resultado = imc
+        this.setState(state)
 
-    } else if ( state.resultado < 17 ){
-      state.resultadoText = "Magreza Moderada"
+        if ( state.resultado < 16 ){
+          state.resultadoText = "Magreza Grave"
 
-    } else if ( state.resultado < 18.5 ){
-      state.resultadoText = "Magreza Leve"
+        } else if ( state.resultado < 17 ){
+          state.resultadoText = "Magreza Moderada"
 
-    } else if ( state.resultado < 25 ){
-      state.resultadoText = "Saudável"
+        } else if ( state.resultado < 18.5 ){
+          state.resultadoText = "Magreza Leve"
 
-    } else if ( state.resultado < 30 ){
-      state.resultadoText = "Sobrepeso"
+        } else if ( state.resultado < 25 ){
+          state.resultadoText = "Saudável"
 
-    } else if ( state.resultado < 35 ){
-      state.resultadoText = "Obesidade Grau I"
+        } else if ( state.resultado < 30 ){
+          state.resultadoText = "Sobrepeso"
 
-    } else if ( state.resultado < 40 ){
-      state.resultadoText = "Obesidade Grau II"
-    
-    }else {
-      state.resultadoText = "Obesidade Grau III"
-    
+        } else if ( state.resultado < 35 ){
+          state.resultadoText = "Obesidade Grau I"
+
+        } else if ( state.resultado < 40 ){
+          state.resultadoText = "Obesidade Grau II"
+        
+        }else {
+          state.resultadoText = "Obesidade Grau III"
+        
+        }
+      } else {
+        Alert.alert(
+          'Preenchimento dos Campos',
+          'Por favor, os campos massa e altura devem ser preenchidos somente com números e um ponto, se necessário. \n\n' + 
+          'Exemplo: \n\n' +
+          'Massa: 81 ou 85.8 \n' +
+          'Altura: 1.80 ou 2',
+          [
+            {text: 'OK', onPress: () => console.log('OK Pressed')},
+          ],
+          { cancelable: false }
+        )
+      }
+
+    } else {
+      Alert.alert(
+        'Campos Obrigatórios',
+        'Por favor, preencha os campos massa e altura.',
+        [
+          {text: 'OK', onPress: () => console.log('OK Pressed')},
+        ],
+        { cancelable: false }
+      )
     }
 
   }
@@ -64,8 +94,14 @@ export default class App extends Component<Props> {
     return (
       <View style={styles.container}>
         <View style={styles.entradas}>
-          <TextInput placeholder="Massa" keyboardType="numeric" style={styles.input} onChangeText={(massa) => {this.setState({massa})}}/>
-          <TextInput placeholder="Altura" keyboardType="numeric" style={styles.input} onChangeText={(altura) => {this.setState({altura})}}/>
+          <View style={styles.entrada}>
+            <TextInput placeholder="Massa" keyboardType="numeric" style={styles.input} onChangeText={(massa) => {this.setState({massa})}}/>
+            <Text style={styles.inputText}>Kg</Text>
+          </View>
+          <View style={styles.entrada}>
+            <TextInput placeholder="Altura" keyboardType="numeric" style={styles.input} onChangeText={(altura) => {this.setState({altura})}}/>
+            <Text style={styles.inputText}>M</Text>
+          </View>
         </View>
         <TouchableOpacity style={styles.button} onPress={this.calcular}>
           <Text style={styles.buttonText}>Calcular</Text>
@@ -85,16 +121,25 @@ const styles = StyleSheet.create({
   entradas: {
     flexDirection: 'row'
   },
+  entrada: {
+    flexDirection: 'row',
+    width: "50%"
+  },
   button: {
     backgroundColor: "#89ffa5"
   },
   input:{
     height: 80,
-    textAlign: "center",
-    width: "50%",
-    fontSize: 50,
-    marginTop: 24,
+    textAlign: "right",
+    fontSize: 40,
+    marginTop: 15,
     color: "gray"
+  },
+  inputText:{
+    height: 80,
+    fontSize: 30,
+    marginTop: 35,
+    color: "lightgray"
   },
   buttonText: {
     alignSelf: 'center',
